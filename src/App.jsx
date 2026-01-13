4bfde57c4b249e878cc1945305340f5aaafa910c
khyarmt <khyarmt@gmail.com>
2026-01-13 18:18:10 +0900

    Update on progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import mapboxgl from "mapbox-gl";
+import * as d3 from "d3";
 
 const INITIAL_CENTER = [-74.0242, 40.6941];
 const INITIAL_ZOOM = 10.12;
@@ -15,6 +16,7 @@ function GeoScatterplotContent({ width, height }) {
     mapboxgl.accessToken = mapboxAccessToken;
     mapRef.current = new mapboxgl.Map({
       container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/light-v11",
       center: center,
       zoom: zoom,
     });
@@ -84,7 +86,6 @@ function GeoScatterplot() {
       observer.disconnect();
     };
   }, []);
-  console.log(size.width, size.height);
 
   return (
     <div
@@ -97,6 +98,42 @@ function GeoScatterplot() {
 }
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("data/airports.csv");
+      const data = d3.csvParse(await response.text());
+      for (const item of data) {
+        item.airportId = Number(item.airport_id);
+        item.altitude = Number(item.altitude);
+        item.latitude = Number(item.latitude);
+        item.longitude = Number(item.longitude);
+        item.tzDatabaseTimezone = item.tz_database_timezone;
+        delete item.airport_id;
+        delete item.tz_database_timezone;
+      }
+
+      // array to GeoJSON
+      const geoJson = {};
+      geoJson.type = "FeatureCollection";
+      geoJson.features = [];
+      for (const item of data) {
+        const feature = {};
+        feature.type = "Feature";
+        feature.geometry = {
+          type: "Point",
+          coordinates: [item.longitude, item.latitude],
+        };
+        feature.properties = {};
+      }
+      console.log(geoJson);
+
+      setData(data);
+    })();
+  }, []);
+  console.log(data);
+
   return (
     <div style={{ position: "absolute", top: 0, right: 0, bottom: 0, left: 0 }}>
       <GeoScatterplot />
